5507b011dd1421f6bc94cf6c83c6fc7ea925c8a8
yeti8912 <cog_iceman@hotmail.com>
2016-05-11 12:44:48 -0500

    New

---
+++ Source/CardsOfWar/CardsOfWarGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "CardsOfWarGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARDSOFWAR_API ACardsOfWarGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
